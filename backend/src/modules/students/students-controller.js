8a34295e9000a7e8e9aa96f18c27cc2ff612021a
Gattamaneni Kumar Raja <raja@bhrish.com>
2025-03-18 02:35:26 +0000

    CRUD changes

--- backend/src/modules/students/students-controller.js
+++ backend/src/modules/students/students-controller.js
@@ -3,26 +3,42 @@ const { getAllStudents, addNewStudent, getStudentDetail, setStudentStatus, updat
 
 const handleGetAllStudents = asyncHandler(async (req, res) => {
     //write your code
+    const students = await getAllStudents();
+    res.json({ students });
 
 });
 
 const handleAddStudent = asyncHandler(async (req, res) => {
     //write your code
+    const { payload } = req.body;
+    const message = await addNewStudent({ payload });
+    res.json(message);
 
 });
 
 const handleUpdateStudent = asyncHandler(async (req, res) => {
     //write your code
+    const { payload } = req.body;
+    const message = await updateStudent({ payload });
+    res.json(message);
 
 });
 
 const handleGetStudentDetail = asyncHandler(async (req, res) => {
     //write your code
+    const { id } = req.params;
+    const detail = await getStudentDetail(id);
+    res.json(detail);
 
 });
 
 const handleStudentStatus = asyncHandler(async (req, res) => {
     //write your code
+    const { id } = req.params;
+    const { id: reviewer_id} = req.user;
+    const { status } = req.body;
+    const message = await setStudentStatus({ id, reviewer_id, status });
+    res.json(message);
 
 });
 
